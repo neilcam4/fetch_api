cbbde301e37337a08765fbcb41583e15d97a9d99
Neil Campbell <neildcampbell@hotmail.com>
2019-10-20 07:24:15 +0100

    added new button

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 
 
-document.getElementById('button1').addEventListener('click',getScript )
+document.getElementById('button1').addEventListener('click',getText )
 
-function getScript(){
+function getText(){
     fetch('file.txt')
     .then(function(response){
-        console.log(response)
+       response.text()
+       console.log(response.text())
+    })
+    .then(function(data){
+        console.log(data)
     })
     .catch(function error(){
         console.log("oh no")
